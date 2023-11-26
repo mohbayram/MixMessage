3cdc71db649f496b650eafb3a2f8057b4c4212c5
mohamad <mohamadbayram52@gmail.com>
2023-11-26 18:41:56 +0000

    initialize list of messages

--- mixmessage.js
+++ mixmessage.js
@@ -1,1 +1,21 @@
-console.log("hello world!")
+
+// Make a list of Messages 
+const messages = 
+            [
+                {author: "Queen Elizabeth II", 
+                message:"It is often the small steps, not the giant leaps, that bring about the most lasting change."},
+                {author: "Nelson Mandela", 
+                message:"Education is the most powerful weapon which you can use to change the world."},
+                {author: "Amanda Gorman", 
+                message:"There is always light. If only we're brave enough to see it. If only we're brave enough to be it."},
+                {author: "Booker T. Washington", 
+                message:"If you want to lift yourself up, lift up someone else."},
+                {author: "Cicely Tyson", 
+                message:"I have learned not to allow rejection to move me."},
+                {author: "André Leon Talley", 
+                message:"I scorched the earth with my talent and I let my light shine."},
+                {author: "Madeleine Albright", 
+                message:"It took me quite a long time to develop a voice, and now that I have it, I am not going to be silent."}
+            ]
+
+console.log(messages[0].author)
